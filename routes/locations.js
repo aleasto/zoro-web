3ee4f2ed3e10e44d66d89424ff74858400d7e014
Alessandro Astone <ales.astone@gmail.com>
2020-07-20 23:57:12 +0200

    hookup mongodb

--- routes/locations.js
+++ routes/locations.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET locations listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+let db;
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(process.env.MONGODB_URI)
+.then(client => db = client.db("zorodb"))
+.catch(err => {
+  console.error(err);
+  process.exit(1);
 });
 
-router.post('/', function(req, res) {
-  console.log(req.body);
-  res.status(200).send("Gotcha");
+/* GET locations listing. */
+router.get('/', function(req, res, next) {
+  db.collection("locations").find()
+  .toArray()
+  .then(items => res.status(200).send(items))
+  .catch(next)
+});
+
+/* POST a new location */
+router.post('/', function(req, res, next) {
+  let inObj = req.body;
+  let dbObj = { date: inObj.date, lat: inObj.lat, lon: inObj.lon, acc: inObj.acc };
+  db.collection("locations").insertOne(dbObj)
+  .then(() => res.status(200).send("Location saved"))
+  .catch(next);
 });
 
 module.exports = router;
