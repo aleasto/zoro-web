bef943f3758a7927d5a4094681af35ea26bdf980
Alessandro Astone <ales.astone@gmail.com>
2020-07-20 21:57:36 +0200

    create locations endpoint

--- routes/locations.js
+++ routes/locations.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET locations listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', function(req, res) {
+  console.log(req.body);
+  res.status(200).send("Gotcha");
 });
 
 module.exports = router;
