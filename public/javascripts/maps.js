ccaf6ea014637fcf90931bfb9195866a6cc17328
Alessandro Astone <ales.astone@gmail.com>
2020-07-28 15:18:44 +0200

    select circles and show info box

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,4 +1,5 @@
 let map;
+let infoWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -12,9 +13,11 @@ function initMap() {
     };
   });
   
+  infoWindow = new google.maps.InfoWindow();
   loadTrackerData();
 }
 
+let lastSelectedCircle;
 function addCirlce(feature) {
   feature.circle = new google.maps.Circle({
     map,
@@ -26,10 +29,35 @@ function addCirlce(feature) {
     strokeWeight: .5,
     zIndex: feature.getProperty('fix_time')
   });
+  
+  feature.clickListener = google.maps.event.addListener(feature.circle, 'click', () => {
+    if (lastSelectedCircle) {
+      lastSelectedCircle.setOptions({
+        strokeColor: 'white',
+      });
+    }
+    feature.circle.setOptions({
+      strokeColor: 'black',
+    });
+    let geometry = feature.getGeometry().get();
+    infoWindow.setContent([
+      "<h2>" + new Date(feature.getProperty('fix_time')).toLocaleString() + "</h2>",
+      "Reported at: " + new Date(feature.getProperty('report_time')).toLocaleString(),
+      "Altitude: " + feature.getProperty('alt').toFixed(1),
+      "Accuracy: " + '±' + feature.getProperty('acc').toFixed(1) + 'm',
+      "Battery: " + feature.getProperty('bat') + '%',
+      "Network: " + feature.getProperty('net'),
+      `<a target="_blank" rel="noopener noreferrer"
+        href="https://www.google.com/maps/search/?api=1&query=${geometry.lat()},${geometry.lng()}">View on Google Maps</a>`,
+    ].join('<div>'));
+    infoWindow.setPosition(feature.getGeometry().get());
+    infoWindow.open(map);
+    lastSelectedCircle = feature.circle;
+  })
 }
 
 function loadTrackerData() {
-  map.data.forEach(feature => map.data.remove(feature));
+  clearData();
   map.data.loadGeoJson("/locations?acc=16&geojson", null, () => {
     let bounds = new google.maps.LatLngBounds(); 
     let lastSeen;
@@ -44,11 +72,20 @@ function loadTrackerData() {
     });
 
     lastSeen.circle.setOptions({
-      strokeColor: 'black',
+      fillColor: 'yellow',
+      fillOpacity: 0.5,
       strokeWeight: 2,
     });
 
     map.fitBounds(bounds);
-    map.setZoom(18);
+    map.setZoom(map.getZoom() - 4);
+  });
+}
+
+function clearData() {
+  map.data.forEach(feature => {
+    google.maps.event.clearListeners(feature.circle, 'click');
+    feature.circle.setMap(null);
+    map.data.remove(feature);
   });
 }
